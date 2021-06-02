cca39821478ba784768d402bac2322785f585938
Harshana Rathnayaka <huratnayaka@gmail.com>
2021-06-02 17:04:45 +0530

    project init

---
+++ android/app/src/main/kotlin/com/example/my_archive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_archive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:my_archive/components/theme.dart';
+import 'package:my_archive/providers/GoogleSignInProvider.dart';
+import 'package:my_archive/screens/Dashboard.dart';
+import 'package:my_archive/screens/Login.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeNotifier(),
+      child: Consumer<ThemeNotifier>(
+          builder: (context, ThemeNotifier notifier, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          debugShowCheckedModeBanner: false,
+          theme: notifier.isDark ? dark : light,
+          home: ChangeNotifierProvider(
+            create: (_) => GoogleSignInProvider(),
+            child: StreamBuilder(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  final provider = Provider.of<GoogleSignInProvider>(context);
+
+                  if (provider.isSigningIn) {
+                    return Scaffold(
+                      body: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    );
+                  } else if (snapshot.hasData) {
+                    return Dashboard();
+                  } else {
+                    return Login();
+                  }
+                }),
+          ),
+          routes: <String, WidgetBuilder>{
+            Login.tag: (BuildContext context) => Login(),
+            Dashboard.tag: (BuildContext context) => Dashboard(),
+          },
+        );
+      }),
+    );
+  }
+}
