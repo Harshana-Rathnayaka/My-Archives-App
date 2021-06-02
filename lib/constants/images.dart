2c96874ed1570babbf5d60cb5c049ec785a4aa64
Harshana Rathnayaka <huratnayaka@gmail.com>
2021-06-02 17:06:55 +0530

    images added

---
+++ lib/constants/images.dart
@@ -0,0 +1,5 @@
+const movies = 'assets/images/movies.svg';
+const tv_series = 'assets/images/tv_series.svg';
+const watchlist = 'assets/images/watchlist.svg';
+const places = 'assets/images/places.svg';
+const google = 'assets/images/google.svg';
