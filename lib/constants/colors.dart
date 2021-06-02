7867db12bc6a6354cc2e2fa833691510ce49f938
Harshana Rathnayaka <huratnayaka@gmail.com>
2021-06-02 21:46:45 +0530

    new dark mode switch

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,3 +3,16 @@ import 'package:flutter/material.dart';
 const lightPrimaryColor = Color(0xFF008080);
 const lightAppBarColor = Color(0xFF008080);
 const lightScaffoldColor = Color(0xFF48a999);
+const colorWhite = Color(0xFFE7F2F8);
+const colorRed = Color(0xFFDE3C4B);
+const colorGreen = Color(0xFF00C22A);
+const colorBlack = Color(0xFF000000);
+
+// colors for the dark mode switch GitHub style
+const activeToggleColor = Color(0xFF6E40C9);
+const inactiveToggleColor = Color(0xFF2F363D);
+const activeSwitchBorder = Color(0xFF3C1E70);
+const inactiveSwitchBorder = Color(0xFFD1D5DA);
+const activeColor = Color(0xFF271052);
+const activeIcon = Color(0xFFF8E3A1);
+const inactiveIcon = Color(0xFFFFDF5D);
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/material.dart';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:my_archive/components/theme.dart';
-import 'package:my_archive/constants/fonts.dart';
-import 'package:my_archive/providers/google_signin_provider.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:provider/provider.dart';
+
+import '../components/theme.dart';
+import '../constants/colors.dart';
+import '../constants/fonts.dart';
+import '../providers/google_signin_provider.dart';
 
 class CustomDrawer extends StatefulWidget {
   @override
@@ -15,19 +19,22 @@ class _CustomDrawerState extends State<CustomDrawer> {
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
-    return Drawer(
-      child: SafeArea(
-        child: Material(
-          child: ListView(
-            padding: EdgeInsets.symmetric(horizontal: 20.0),
-            children: [
-              InkWell(
-                child: Container(
-                  padding: EdgeInsets.only(top: 40.0, bottom: 20.0),
+    return Consumer<ThemeNotifier>(
+      builder: (context, notifier, child) => Drawer(
+        child: SafeArea(
+          child: Material(
+            child: ListView(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 40.0, horizontal: 20.0),
+              children: [
+                Container(
+                  color: Theme.of(context).canvasColor,
+                  margin: EdgeInsets.only(bottom: 40.0),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       CircleAvatar(
-                        backgroundColor: Colors.white,
+                        backgroundColor: Theme.of(context).accentColor,
                         radius: 26,
                         child: CachedNetworkImage(
                           imageUrl: user.photoURL,
@@ -48,74 +55,92 @@ class _CustomDrawerState extends State<CustomDrawer> {
                               new Icon(Icons.error, size: 26),
                         ),
                       ),
-                      SizedBox(width: 20),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(user.displayName,
-                              style: TextStyle(
-                                  fontFamily: fontRegular,
-                                  fontWeight: FontWeight.bold)),
-                          Text(
-                            user.email,
-                            style: TextStyle(
-                                fontFamily: fontRegular,
-                                fontSize: textSizeSmall),
+                      Expanded(
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                user.displayName,
+                                style: TextStyle(
+                                    fontFamily: fontRegular,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                user.email,
+                                style: TextStyle(
+                                    fontFamily: fontRegular,
+                                    fontSize: textSizeSmall),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                            ],
                           ),
-                        ],
-                      )
+                        ),
+                      ),
+                      FlutterSwitch(
+                        duration: Duration(milliseconds: 800),
+                        width: 50.0,
+                        height: 24.0,
+                        toggleSize: 20.0,
+                        value: notifier.isDark,
+                        borderRadius: 14.0,
+                        padding: 1.0,
+                        activeToggleColor: activeToggleColor,
+                        inactiveToggleColor: inactiveToggleColor,
+                        activeSwitchBorder:
+                            Border.all(color: activeSwitchBorder, width: 1.0),
+                        inactiveSwitchBorder:
+                            Border.all(color: inactiveSwitchBorder, width: 1.0),
+                        activeColor: activeColor,
+                        inactiveColor: colorWhite,
+                        activeIcon:
+                            Icon(Icons.nightlight_round, color: activeIcon),
+                        inactiveIcon: Icon(Icons.wb_sunny, color: inactiveIcon),
+                        onToggle: (value) {
+                          notifier.toggleTheme();
+                        },
+                      ),
                     ],
                   ),
                 ),
-              ),
-              Divider(),
-              SizedBox(height: 16),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () {},
-              ),
-              SizedBox(height: 16),
-              ListTile(
-                leading: Icon(Icons.question_answer),
-                title: Text('FAQs'),
-              ),
-              SizedBox(height: 16),
-              ListTile(
-                leading: Icon(Icons.help),
-                title: Text('How to use'),
-              ),
-              SizedBox(height: 16),
-              Consumer<ThemeNotifier>(
-                builder: (context, notifier, child) => SwitchListTile(
-                  secondary: Icon(
-                      notifier.isDark ? Icons.nights_stay : Icons.wb_sunny,
-                      color: notifier.isDark ? Colors.yellow : null),
-                  title: Text(notifier.isDark ? "Light Mode" : "Dark Mode"),
-                  value: notifier.isDark,
-                  onChanged: (value) {
-                    notifier.toggleTheme();
+                Divider(thickness: 1.0),
+                SizedBox(height: 16),
+                ListTile(
+                  leading: Icon(Icons.settings),
+                  title: Text('Settings'),
+                  onTap: () {},
+                ),
+                SizedBox(height: 16),
+                ListTile(
+                  leading: Icon(Icons.question_answer),
+                  title: Text('FAQs'),
+                ),
+                SizedBox(height: 16),
+                ListTile(
+                  leading: Icon(Icons.help),
+                  title: Text('How to use'),
+                ),
+                SizedBox(height: 16),
+                ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text('About'),
+                ),
+                SizedBox(height: 24),
+                Divider(thickness: 1.0),
+                SizedBox(height: 16),
+                ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text('Logout'),
+                  onTap: () {
+                    final provider = Provider.of<GoogleSignInProvider>(context,
+                        listen: false);
+                    provider.logout();
                   },
                 ),
-              ),
-              SizedBox(height: 24),
-              Divider(),
-              SizedBox(height: 24),
-              ListTile(
-                leading: Icon(Icons.info),
-                title: Text('About'),
-              ),
-              SizedBox(height: 16),
-              ListTile(
-                leading: Icon(Icons.exit_to_app),
-                title: Text('Logout'),
-                onTap: () {
-                  final provider =
-                      Provider.of<GoogleSignInProvider>(context, listen: false);
-                  provider.logout();
-                },
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
