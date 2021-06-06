d9ff3dd5bf6d2568e895addac3c236d6daf6ad1f
Harshana Rathnayaka <huratnayaka@gmail.com>
2021-06-06 06:19:02 +0530

    added tooltips

--- lib/widgets/dashboard_card.dart
+++ lib/widgets/dashboard_card.dart
@@ -6,10 +6,12 @@ class DashboardCard extends StatelessWidget {
   final IconData displayIcon;
   final String title;
   final Function onPressed;
+  final String tooltip;
   const DashboardCard({
     @required this.displayIcon,
     @required this.title,
     @required this.onPressed,
+    @required this.tooltip,
     Key key,
   }) : super(key: key);
 
@@ -17,29 +19,33 @@ class DashboardCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onPressed,
-      child: Card(
-        elevation: 10,
-        child: Container(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
+      child: Tooltip(
+        message: tooltip,
+        child: Card(
+          elevation: 10,
+          child: Container(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
                   padding: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       color: Theme.of(context).accentColor.withOpacity(0.2)),
                   child: Icon(displayIcon,
-                      color: Theme.of(context).accentColor, size: 28)),
-              SizedBox(height: 10),
-              Text(
-                title,
-                style: TextStyle(
-                    letterSpacing: 1.0,
-                    fontFamily: fontRegular,
-                    fontSize: textSizeLargeMedium),
-              )
-            ],
+                      color: Theme.of(context).accentColor, size: 28),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  title,
+                  style: TextStyle(
+                      letterSpacing: 1.0,
+                      fontFamily: fontRegular,
+                      fontSize: textSizeLargeMedium),
+                )
+              ],
+            ),
           ),
         ),
       ),
