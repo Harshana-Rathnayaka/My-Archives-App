12373783e46c23ded94279ff1b62d0917e4df15f
Harshana Rathnayaka <huratnayaka@gmail.com>
2022-06-19 02:23:26 +0530

    intl package added for number formatting

--- lib/utils/helper_methods.dart
+++ lib/utils/helper_methods.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 
 import '../constants/colors.dart';
 import '../constants/fonts.dart';
@@ -45,4 +45,11 @@ SnackBar customSnackbar({required IconData icon, required Color iconColor, requi
   );
 }
 
-void showToast({required String msg, required Color backGroundColor}) => Fluttertoast.showToast(msg: msg, textColor: colorWhite, backgroundColor: backGroundColor, toastLength: Toast.LENGTH_LONG);
+void showToast({required String msg, required Color backGroundColor}) => Fluttertoast.showToast(
+      msg: msg,
+      textColor: colorWhite,
+      backgroundColor: backGroundColor,
+      toastLength: Toast.LENGTH_LONG,
+    );
+
+NumberFormat numberFormat = NumberFormat('###,###.##');
