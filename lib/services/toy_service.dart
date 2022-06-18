f2314ac86ab32c8bd5a5b284d5812996d0092119
Harshana Rathnayaka <huratnayaka@gmail.com>
2022-06-18 05:33:18 +0530

    modified the toy service to use Toy model when sending and retrieving toy data from firebase

--- lib/services/toy_service.dart
+++ lib/services/toy_service.dart
@@ -1,15 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../models/toy.dart';
+
 class ToyService {
   final String uid;
-  final CollectionReference toys = FirebaseFirestore.instance.collection('toys');
+  final CollectionReference toysReference = FirebaseFirestore.instance.collection('toys');
+  late CollectionReference toyCollectionReference = toysReference.doc(uid).collection('toyCollection').withConverter<Toy>(
+        fromFirestore: (snapshots, _) => Toy.fromJson(snapshots.data()!),
+        toFirestore: (toy, _) => toy.toJson(),
+      );
 
   ToyService({required this.uid});
 
-// add to toys collection
-  Future addToy({required Map<String, dynamic> toyDetails}) async {
+  // getting the toy collection for each user
+  getToyCollectionStream() => toyCollectionReference.snapshots();
+
+  // add to toys collection
+  Future addToy({required Toy toyDetails}) async {
     String uniqueId = DateTime.now().microsecondsSinceEpoch.toString();
-    CollectionReference toyCollection = toys.doc(uid).collection('toyCollection');
-    await toyCollection.doc(uniqueId).set(toyDetails);
+    await toyCollectionReference.doc(uniqueId).set(toyDetails);
   }
 }
