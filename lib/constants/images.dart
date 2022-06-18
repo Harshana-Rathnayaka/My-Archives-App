5cd039c29e2f1a1b9a9953b6539e8bfa0c088fce
Harshana Rathnayaka <huratnayaka@gmail.com>
2022-06-18 13:30:04 +0530

    image to display when image could not be loaded from firebase

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,3 +1,4 @@
 const movies = 'assets/images/movies.svg';
 const google = 'assets/images/google.svg';
 const empty = 'assets/images/empty.svg';
+const brokenImage = 'assets/images/broken-image.png';
